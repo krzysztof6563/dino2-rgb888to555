2de6cf6dff6cc5cd168ff8396a93b8058a0c367a
Krzysztof Michalski <krzysztofmichalski42@gmail.com>
2018-09-01 23:45:00 +0200

    Added comments

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ int main(int argc, char* argv[])
     uint8_t outByte = 0x0, outByte2 = 0x0;
     for (int i=1;i<argc;i++){
         inFile.open(argv[i], std::fstream::binary);
+        //Removing .data from filename
         char output[5000];
         strncpy(output, argv[i], strlen(argv[i])-5);
         output[strlen(argv[i])-5] = '\0';
@@ -27,23 +28,26 @@ int main(int argc, char* argv[])
                 byte2 = 0;
                 byte3 = 0;
                 byte4 = 0;
+                //Reading RGB bytes
                 inFile.read(&byte1, 1);
                 inFile.read(&byte2, 1);
                 inFile.read(&byte3, 1);
                 //inFile.read(&byte4, 1);
+                //Dropping 3 least important bits
                 red5 = byte1 >> 3;
                 red5 = red5 & 0b00011111;
                 green5 = byte2 >> 3;
                 green5 = green5 & 0b00011111;
                 blue5 = byte3 >> 3;
                 blue5 = blue5 & 0b00011111;
+                //Combining invidual bits to "Dino Crisis 2" format
                 outByte = 0;
                 outByte2 = 0;
                 outByte = outByte | red5;
                 outByte = outByte | green5 << 5;
                 outByte2 = outByte2 | green5 >> 3;
                 outByte2 = outByte2 | blue5 << 2;
-
+                //Writing to file
                 outFile.write(reinterpret_cast<const char *>(&outByte), sizeof (outByte));
                 outFile.write(reinterpret_cast<const char *>(&outByte2), sizeof (outByte2));
                 pixelNumber++;
